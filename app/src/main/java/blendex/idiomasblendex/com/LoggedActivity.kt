07b82dc828755586e313a02c74aef859f3954570
Carlos Serpa <casoser@gmail.com>
2019-06-27 17:42:41 -0500

    Probando SELECCION DE PROGRAMA

--- app/src/main/java/blendex/idiomasblendex/com/LoggedActivity.kt
+++ app/src/main/java/blendex/idiomasblendex/com/LoggedActivity.kt
@@ -1,12 +1,52 @@
 package blendex.idiomasblendex.com
 
+import android.graphics.Color
+import android.os.AsyncTask
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import blendex.idiomasblendex.com.db.AppDatabase
+import blendex.idiomasblendex.com.db.objects.Programa_db
+import kotlinx.android.synthetic.main.activity_logged.*
+import org.jetbrains.anko.AlertBuilder
+import org.jetbrains.anko.AlertBuilderFactory
+import org.jetbrains.anko.selector
 
 class LoggedActivity : AppCompatActivity() {
+
+    private var db: AppDatabase? = null
+    var ListPrograms = listOf<Programa_db>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logged)
+        intent?.let {
+
+            txName.text = "Bienvenido\n${it.extras.getString("n")
+                                            .toLowerCase().capitalize().trim()}"
+        }
+        db = AppDatabase.getInstance(this)!!
+        GetPrograms(this).execute()
+        CardProgram.setOnClickListener {
+            var p = mutableListOf("")
+            for (programs in ListPrograms){
+                p.add(programs.programas)
+            }
+
+            selector("", p) { dialogInterface, i ->
+                currentProgram.text = p[i]
+            }
+        }
+    }
+
+
+    private class GetPrograms(var context: LoggedActivity) : AsyncTask<Void, Void, List<Programa_db>>() {
+        override fun doInBackground(vararg params: Void?): List<Programa_db> {
+            return context.db!!.programDao().getAll()
+        }
+        override fun onPostExecute(result: List<Programa_db>) {
+            context.ListPrograms = result
+        }
     }
 }
--- app/src/main/java/blendex/idiomasblendex/com/SplashActivity.kt
+++ app/src/main/java/blendex/idiomasblendex/com/SplashActivity.kt
@@ -7,11 +7,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import blendex.idiomasblendex.com.db.AppDatabase
+import blendex.idiomasblendex.com.db.objects.Programa_db
 import blendex.idiomasblendex.com.db.objects.Student_db
+import org.jetbrains.anko.startActivity
+import java.io.Serializable
 
 class SplashActivity : AppCompatActivity() {
     private var db: AppDatabase? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -27,7 +29,6 @@ class SplashActivity : AppCompatActivity() {
         val splashScreenDuration = 500L
         Handler().postDelayed(
             {
-                // After the splash screen duration, route to the right activities
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
@@ -44,12 +45,30 @@ class SplashActivity : AppCompatActivity() {
         override fun onPostExecute(chapterList: List<Student_db>?) {
             if (chapterList!!.isNotEmpty()) {
                 Log.w("caso","este esta logeado${chapterList[0].nombres}-${chapterList[0].apellidos}")
-                context.scheduleSplashScreen()
+                context.scheduleLogged(chapterList[0].nombres,chapterList[0].apellidos)
             }else{
                 context.scheduleSplashScreen()
             }
 
         }
     }
+
+
+    fun scheduleLogged(nom: String, ap: String) {
+        val splashScreenDuration =200L
+        Handler().postDelayed(
+            {
+                // After the splash screen duration, route to the right activities
+                //val intent = Intent(this, LoggedActivity::class.java)
+
+                //startActivity(intent)
+                //finish()
+                startActivity<LoggedActivity>("n" to nom,"a" to ap)
+
+            },
+            splashScreenDuration
+        )
+    }
+
 
 }
