00619bcb417799b93c603f904f7910553895f196
Carlos Serpa <casoser@gmail.com>
2019-06-27 19:09:24 -0500

    Implementando epoxy

--- app/src/main/java/blendex/idiomasblendex/com/LoggedActivity.kt
+++ app/src/main/java/blendex/idiomasblendex/com/LoggedActivity.kt
@@ -1,10 +1,12 @@
 package blendex.idiomasblendex.com
 
+import android.content.Context
 import android.graphics.Color
 import android.os.AsyncTask
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout.JUSTIFICATION_MODE_INTER_WORD
 import androidx.appcompat.app.AlertDialog
 import blendex.idiomasblendex.com.db.AppDatabase
 import blendex.idiomasblendex.com.db.objects.Programa_db
@@ -23,18 +25,19 @@ class LoggedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_logged)
         intent?.let {
 
-            txName.text = "Bienvenido\n${it.extras.getString("n")
+            txName.text = "${it.extras.getString("n")
                                             .toLowerCase().capitalize().trim()}"
         }
+
         db = AppDatabase.getInstance(this)!!
         GetPrograms(this).execute()
-        CardProgram.setOnClickListener {
+
+        LinearProgram.setOnClickListener {
             var p = mutableListOf("")
             for (programs in ListPrograms){
-                p.add(programs.programas)
+                p.add(programs.programas.replace(" - "," ").toLowerCase().capitalize())
             }
-
-            selector("", p) { dialogInterface, i ->
+            selector("Selecciona el programa actual", p) { dialogInterface, i ->
                 currentProgram.text = p[i]
             }
         }
@@ -46,6 +49,7 @@ class LoggedActivity : AppCompatActivity() {
             return context.db!!.programDao().getAll()
         }
         override fun onPostExecute(result: List<Programa_db>) {
+            context.currentProgram.text = result[0].programas.replace(" - "," ").toLowerCase().capitalize().trim()
             context.ListPrograms = result
         }
     }
