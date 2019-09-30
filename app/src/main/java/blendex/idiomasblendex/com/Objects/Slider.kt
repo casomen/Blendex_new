c6a23ac0c261ccd3f578a10a0d594543df092ae0
Carlos Serpa <casoser@gmail.com>
2019-09-30 17:26:18 -0500

    nati

--- app/src/main/java/blendex/idiomasblendex/com/Objects/Slider.kt
+++ app/src/main/java/blendex/idiomasblendex/com/Objects/Slider.kt
@@ -1,3 +1,6 @@
 package blendex.idiomasblendex.com.Objects
 
-data class Slider(val nameImage: String, var urlImage:String, var urlWeb:String, var color:String)
+data class Slider(val nameImage: String,
+                  var urlImage:String,
+                  var urlWeb:String,
+                  var color:String)
